8379e5ae0edf029a5cee041412a3e155845ea9a9
Tatiana-goit <tanchou74@gmail.com>
2021-06-12 23:18:56 +0300

    Начало

--- src/js/light-box.js
+++ src/js/light-box.js
@@ -1,9 +1,17 @@
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
 
+import { ligtboxSpinner } from './spinner';
+
 export default function onLightboxOpen(e) {
   if (e.target.nodeName !== 'IMG') {
     return;
   }
   basicLightbox.create(`<img src="${e.target.dataset.src}">`).show();
 };
+
+ // Запуск спиннера
+ ligtboxSpinner.spin(lightboxRef);
+
+  // Остановка спиннера при загрузке
+  e.target.onload = () => ligtboxSpinner.stop();
